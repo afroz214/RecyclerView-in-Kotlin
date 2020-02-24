41a3d472e6ac5a45e60f2132644d1e40671b5f69
Your Name <you@example.com>
2020-02-25 02:30:19 +0530

    i have added an image view at xml file and fetch that image in ListAdapter

--- app/src/main/java/com/example/amandahinchman_dominguez/recyclerviews/ListAdapter.kt
+++ app/src/main/java/com/example/amandahinchman_dominguez/recyclerviews/ListAdapter.kt
@@ -2,6 +2,7 @@ package com.example.amandahinchman_dominguez.recyclerviews
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -26,16 +27,19 @@ class MovieViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
     RecyclerView.ViewHolder(inflater.inflate(R.layout.list_item, parent, false)) {
     private var mTitleView: TextView? = null
     private var mYearView: TextView? = null
+    private var mImageView : ImageView? = null
 
 
     init {
         mTitleView = itemView.findViewById(R.id.list_title)
         mYearView = itemView.findViewById(R.id.list_description)
+        mImageView = itemView.findViewById(R.id.image_list)
     }
 
     fun bind(movie: Movie) {
         mTitleView?.text = movie.title
         mYearView?.text = movie.year.toString()
+        mImageView?.setImageResource(movie.image)
     }
 
 }
